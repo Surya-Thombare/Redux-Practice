91080b06b8ad092eb41a60190c700d972ee8126a
Surya-Thombare <suryathombre000@gmail.com>
2024-06-29 00:39:17 +0530

    dispatch

--- index.js
+++ index.js
@@ -6,17 +6,14 @@
     complete: false,
   }
 }
-
 {
   type: 'REMOVE_TODO',
   id: 0,
 }
-
 {
   type: 'TOGGLE_TODO',
   id: 0,
 }
-
 {
   type: 'ADD_GOAL',
   goal: {
@@ -24,32 +21,35 @@
     name: 'Run a Marathon'
   }
 }
-
 {
   type: 'REMOVE_GOAL',
   id: 0
 }
+/*
+Characteristics of a Pure Function
+1) They always return the same result if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
 
-function todos(state = [], action) {
+// Reducer function
+function todos (state = [], action) {
   if (action.type === 'ADD_TODO') {
-    return state.concate([action.todo]);
+    return state.concat([action.todo])
   }
-
   return state
 }
 
-function createStore () {
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
-  // 2. Get the state.
-  // 3. Listen to changes on the state.
-  // 4. Update the state
+  // 2. Get the state. (getState)
+  // 3. Listen to changes on the state. (subscribe)
+  // 4. Update the state (dispatch)
 
   let state
   let listeners = []
-
   const getState = () => state
-
   const subscribe = (listener) => {
     listeners.push(listener)
     return () => {
@@ -57,8 +57,16 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
     subscribe,
+    dispatch,
   }
 }
+
+const store = createStore(todos)
