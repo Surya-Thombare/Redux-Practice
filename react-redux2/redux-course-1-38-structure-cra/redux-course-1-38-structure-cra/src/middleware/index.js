82373b420f2559b4191efd212a6f53b453935b0c
Surya-Thombare <suryathombre000@gmail.com>
2024-07-09 01:12:48 +0530

    folder structure p4

---
+++ react-redux2/redux-course-1-38-structure-cra/redux-course-1-38-structure-cra/src/middleware/index.js
@@ -0,0 +1,13 @@
+import checker from "./checker";
+import logger from "./logger";
+
+import thunk from "redux-thunk";
+
+import { applyMiddleware } from "redux";
+
+
+export default applyMiddleware(
+  thunk,
+  checker,
+  logger
+)
